77365ab15106bb436290aae4fc76ac3c490d1a05
zuuperman <nils.destoop@gmail.com>
2013-09-19 15:20:04 +0200

    Group is not always a boolean

--- lib/CultuurNet/Search/Parameter/Group.php
+++ lib/CultuurNet/Search/Parameter/Group.php
@@ -2,11 +2,9 @@
 
 namespace CultuurNet\Search\Parameter;
 
-class Group extends AbstractBooleanParameter {
+class Group extends AbstractParameter {
   public function __construct($value = TRUE) {
     $this->key = 'group';
-
-    // @todo check if value is a real boolean, otherwise throw an exception
     $this->value = $value;
   }
 }
