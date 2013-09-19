ec9fbf5fe126eb1be2f5ae3ca506d0ee569c5616
zuuperman <nils.destoop@gmail.com>
2013-09-19 16:14:37 +0200

    Get value for booleans

--- lib/CultuurNet/Search/Parameter/Group.php
+++ lib/CultuurNet/Search/Parameter/Group.php
@@ -3,8 +3,20 @@
 namespace CultuurNet\Search\Parameter;
 
 class Group extends AbstractParameter {
+
   public function __construct($value = TRUE) {
     $this->key = 'group';
     $this->value = $value;
   }
+
+  public function getValue() {
+    if ($this->value === TRUE) {
+      return 'true';
+    }
+    elseif ($this->value === FALSE) {
+      return 'false';
+    }
+    return $this->value;
+  }
+
 }
