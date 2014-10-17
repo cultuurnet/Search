062c447075cf61ed39e3b54b024ca1edb395f311
Kristof Coomans <kristof@coomans.eu>
2014-10-17 09:28:28 +0200

    Add Collector class

--- lib/CultuurNet/Search/Guzzle/Parameter/Collector.php
+++ lib/CultuurNet/Search/Guzzle/Parameter/Collector.php
@@ -5,10 +5,33 @@
 
 namespace CultuurNet\Search\Guzzle\Parameter;
 
+use CultuurNet\Search\Parameter\LocalParameterSerializer;
+use CultuurNet\Search\Parameter\Parameter;
+use Guzzle\Common\Collection;
+
 /**
- * Puts arrays of search parameters in a Guzzle Collection, e.g. a Query.
+ * Puts search parameters in a Guzzle Collection, e.g. a Query.
  */
 class Collector
 {
+    /**
+     * @param Parameter[] $parameters
+     * @param Collection $collection
+     */
+    public function addParameters(array $parameters, Collection $collection)
+    {
+        foreach ($parameters as $parameter) {
+            $value = '';
+            $localParams = $parameter->getLocalParams();
+            if (!empty($localParams)) {
+                if (!isset($localParameterSerializer)) {
+                    $localParameterSerializer = new LocalParameterSerializer();
+                }
+                $value = $localParameterSerializer->serialize($localParams);
+            }
 
+            $value .= $parameter->getValue();
+            $collection->add($parameter->getKey(), $value);
+        }
+    }
 } 
