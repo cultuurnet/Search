8cbb55798c60584e7d7b182d62cd26cf283e27f4
Kristof Coomans <kristof@coomans.eu>
2014-10-17 09:27:54 +0200

    Add Collector class

---
+++ lib/CultuurNet/Search/Guzzle/Parameter/Collector.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CultuurNet\Search\Guzzle\Parameter;
+
+/**
+ * Puts arrays of search parameters in a Guzzle Collection, e.g. a Query.
+ */
+class Collector
+{
+
+} 
